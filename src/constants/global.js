6cf004eb8779d43d66d23a428c05f0360ac431f3
Rui Molefas <molefas@gmail.com>
2018-05-12 09:52:12 +0100

    added globals

---
+++ src/constants/global.js
@@ -0,0 +1,12 @@
+/**
+ * Global constants
+ */
+
+export const googleDiscoveryDocs = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+export const googleCalendarScopes = "https://www.googleapis.com/auth/calendar"
+
+// Settings
+export const googleClientId = ''
+export const googleClientSecret = ''
+export const googleApiKey = ''
+export const googleCalendarId = ''
